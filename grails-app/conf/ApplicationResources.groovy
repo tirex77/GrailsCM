8631967926ebb807942eb03efb45883aae057978
tirex77 <tirex77@gmail.com>
2013-10-16 18:51:40 +0400

    waiting home

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,9 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+
+    bootstrap {
+        resource url:'less/custom-bootstrap.less',attrs:[rel: "stylesheet/less", type:'css']
+        dependsOn 'jquery'
+    }
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,7 +59,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.0.0"
         compile ':cache:1.1.1'
-
+        compile ":lesscss-resources:1.3.3"
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.1" // or ":hibernate4:4.1.11.1"
         runtime ":database-migration:1.3.5"
